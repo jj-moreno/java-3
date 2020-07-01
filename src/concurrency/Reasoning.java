f3516b1f78180fa66dd771686bc5d0fa8d2875a3
unknown <moreno7046@hotmail.com>
2020-07-01 09:09:17 -0500

    finished exercise

--- src/concurrency/Reasoning.java
+++ src/concurrency/Reasoning.java
@@ -1,10 +1,19 @@
 package concurrency;
 
-class Reasoning {
+class Reasoning extends Thread {
     //set up this class so it can become a valid thread. 
     void distinguish() {
         //print to the console the difference between a thread and a process
+        System.out.println("A thread is an ordered stream of instructions (smallest unit of execution) that can be run in a program. " + "\n" +
+                "And each program runs in a process which handles the execution of a thread and can multitask by executing multiple threads at the same time (concurrently).");
         //print out you think will happen if you invoke the run() method of a thread as opposed to the start() method of a thread.
+        System.out.println("Not sure but expecting the unexpected. I think we will just execute run() method like a regular method but a thread is not populated." + "\n" +
+                "The start() method does populate a thread and thanks to it we can take advantage of controlling the stream of execution in the program while run() method just runs procedurally");
+    }
+
+    @Override
+    public void run() {
+    distinguish();
     }
 }
 
--- src/concurrency/Sync.java
+++ src/concurrency/Sync.java
@@ -3,31 +3,54 @@ package concurrency;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 class Sync extends Thread {
-    List nums = new ArrayList<>();
+    List<Integer> nums;
 
-    Sync(String name) {
+    Sync(String name, List<Integer> nums) {
         super(name);
+        this.nums = nums;
     }
 
-    @Override 
-    public void run() {
-        Random rand = new Random();
+    @Override
+    synchronized public void run() {
+            Random rand = new Random();
 
-        for(int i=0; i<100; i++) {
-            int randInt = rand.nextInt(100);
-            this.nums.add(randInt);
+            for (int i = 0; i < 100; i++) {
+                int randInt = rand.nextInt(100);
+                this.nums.add(randInt);
+            }
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        Sync syncThread = new Sync("sync thread 1", nums);
+        executor.execute(syncThread);
+        Sync syncThread2 = new Sync("sync thread 2", nums);
+        executor.execute(syncThread2);
+        Sync syncThread3 = new Sync("sync thread 3", nums);
+        executor.execute(syncThread3);
+        Sync syncThread4 = new Sync("sync thread 4", nums);
+        executor.execute(syncThread4);
+        Sync syncThread5 = new Sync("sync thread 5", nums);
+        executor.execute(syncThread5);
+
+        try {
+            executor.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args)  {
-        Sync syncThread = new Sync("sync thread");
-        syncThread.start();
 
         System.out.println(syncThread.nums);
+        executor.shutdown();
         //this prints out an empty list. write some code that will allow the data generated in the syncThread to show up  here.  There is a brute force way and a more sophisticated way.  Either or will work, but strive for sophistication :)
 
     }
-    
+
 }
